e9b612a38466980e5829dd73e43683cbbe60af0d
LDarki <51938450+LDarki@users.noreply.github.com>
2022-02-20 17:15:32 -0300

    Update Form1.cs

--- ResourceTest/Form1.cs
+++ ResourceTest/Form1.cs
@@ -22,7 +22,7 @@ namespace ResourceTest
 
             try
             {
-                if (!webClient.DownloadString("https://github.com/KaizerHind/DemoResourceTest/releases/tag/1.1").Contains("1.1"))
+                if (!webClient.DownloadString("https://raw.githubusercontent.com/KaizerHind/DemoResourceTest/master/Version.txt").Contains("1.1"))
                 {
                     if (MessageBox.Show("Existe una nueva actualizacion! ¿Quieres descargar esta?", "ResourceTest", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                     using (var client = new WebClient())

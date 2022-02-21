d42db4b28ef5aba5b79c107b4be14b59317355c2
KaizerHind <42819568+KaizerHind@users.noreply.github.com>
2022-02-21 17:48:25 -0500

    df

--- ResourceTest/Form1.cs
+++ ResourceTest/Form1.cs
@@ -31,7 +31,7 @@ namespace ResourceTest
 
             try
             {
-                if (!webClient.DownloadString("https://raw.githubusercontent.com/KaizerHind/DemoResourceTest/master/Version.txt").Contains("1.4"))
+                if (!webClient.DownloadString("https://raw.githubusercontent.com/KaizerHind/DemoResourceTest/master/Version.txt").Contains("1.5"))
                 {
                     //if (MessageBox.Show("Existe una nueva actualizacion! ¿Quieres descargar esta?", "ResourceTest", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                     using (var client = new WebClient())

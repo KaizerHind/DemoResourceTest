834f65c94de820708ec4a01ca5b7bb7adf212386
KaizerHind <42819568+KaizerHind@users.noreply.github.com>
2022-02-20 20:53:45 -0500

    Data Files

--- ResourceTest/Form1.Designer.cs
+++ ResourceTest/Form1.Designer.cs
@@ -180,7 +180,7 @@ namespace ResourceTest
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 15;
-            this.textBox1.Text = "1.1";
+            this.textBox1.Text = "1.2";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1

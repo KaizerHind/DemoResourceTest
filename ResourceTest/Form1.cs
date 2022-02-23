edfaf968b165894e88538ca7443994713ccf6e66
KaizerHind <42819568+KaizerHind@users.noreply.github.com>
2022-02-23 12:14:10 -0500

    df

--- ResourceTest/Form1.cs
+++ ResourceTest/Form1.cs
@@ -23,7 +23,7 @@ namespace ResourceTest
 
             try
             {
-                if (!webClient.DownloadString("https://raw.githubusercontent.com/KaizerHind/DemoResourceTest/master/Version.txt").Contains("1.2"))
+                if (!webClient.DownloadString("https://raw.githubusercontent.com/KaizerHind/DemoResourceTest/master/Version.txt").Contains("1.3"))
                 {
                     using (var client = new WebClient())
                     {
